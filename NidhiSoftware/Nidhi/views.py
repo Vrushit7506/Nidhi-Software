233c855eaa5b643acb5911eb2f14e735c903bebc
Vrushit7506 <vrushit7506@gmail.com>
2021-08-06 19:16:51 +0530

    Registration Done

--- NidhiSoftware/Nidhi/views.py
+++ NidhiSoftware/Nidhi/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'Nidhi/register.html', {'form': form})
 
 def home(request):
     return render(request, 'Nidhi/home.html')
